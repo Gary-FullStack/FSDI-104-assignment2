fd6a5a82f4b879c36398a0da510bf24bbdbd0d15
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-27 11:36:14 -0500

    update
    GG

--- js/script.js
+++ js/script.js
@@ -23,32 +23,49 @@ let PetsRus = {
 }
 
 
-function Pet(name, type, age, gender, breed, temperament, demeanor) {
+function Pet(name, age, type, gender, breed, temperament, service) {
     this.name = name;
-    this.type = type;
     this.age = age;
+    this.type = type;
     this.gender = gender;
     this.breed = breed;
     this.temperament = temperament;
-    this.demeanor = demeanor;
+    this.service = service;
 
 }
 
+let inputName = document.getElementById('txtName');
+let inputAge = document.getElementById('txtAge');
+let inputType = document.getElementById('txtType');
+let inputGender = document.getElementById('txtGender');
+let inputBreed = document.getElementById('txtBreed');
+let inputTemperament = document.getElementById('txtTemp');
+let inputService = document.getElementById('txtService');
+
 // register pet to the HTML form
 function register() {
+    console.log(inputName.value, inputAge.value, inputType.value, inputGender.value, inputBreed.value, inputTemperament.value, inputService.value);
+
+    let newPet = new Pet(inputName.value, inputAge.value, inputType.value, inputGender.value, inputBreed.value, inputTemperament.value, inputService.value);
+
+    PetsRus.pets.push(newPet);
+    console.log(newPet);
+    console.log(PetsRus.pets);
+
+
 
 
 
 }
 
 function init() {
-    let Jake = new Pet('Jake', 'dog', '13', 'male', 'lab', 'calm', 'outgoing');
-    let Ivy = new Pet('Ivy', 'dog', '3', 'female', 'mix', 'hyper', 'guarded');
-    let Penny = new Pet('Penny', 'dog', '10', 'female', 'Australian Cattle', 'loving', 'beautiful soul');
+    let Jake = new Pet('Jake', 'dog', '13', 'male', 'lab', 'calm', 'nails');
+    let Ivy = new Pet('Ivy', 'dog', '3', 'female', 'mix', 'hyper', 'grooming');
+    let Penny = new Pet('Penny', 'dog', '10', 'female', 'Australian Cattle', 'loving', 'check-up');
     PetsRus.pets.push(Jake, Ivy, Penny);
-    console.log(PetsRus.pets[0].name);
-    console.log(PetsRus.pets[1].name);
-    console.log(PetsRus.pets[2].name);
+    // console.log(PetsRus.pets[0].name);
+    // console.log(PetsRus.pets[1].name);
+    // console.log(PetsRus.pets[2].name);
 
 }
 window.onload = init;
